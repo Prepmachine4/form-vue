4e602ab6475519a7abe5baa2b47ebd96fd3a30b8
Prepmachine4 <prepmachine4@gmail.com>
2022-06-14 23:26:36 +0800

    签名修复

--- src/views/audit.vue
+++ src/views/audit.vue
@@ -58,6 +58,7 @@
 
 <script setup>
 import {ref} from "vue";
+import vueEsign from 'vue-esign'
 import Data from "@/components/Data";
 import {listAudit, listNeedAudit, postAudit} from "@/api/audit";
 import {useStore} from "vuex";
